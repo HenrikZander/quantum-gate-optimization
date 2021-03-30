177d0cd295685de657a55da5d841a95c9358e540
ElinaSahlberg <79750339+ElinaSahlberg@users.noreply.github.com>
2021-03-30 16:39:57 +0200

    prel. skal av gate fidelity utan M matris

--- GateFidelity.py
+++ GateFidelity.py
@@ -6,12 +6,19 @@ import matplotlib.pyplot as plt
 #from functions import *
 #from plotting import *
 
-#https://arxiv.org/pdf/0910.1315.pdf
-#gate fidellity between noisy implementation and desired gate
-#ExGate existing gate
-#DeGate desired gate
 
-#not tested, not done
+ISWAP=iswap(N=None,targets=[0,1])
 
+avg_gate_fidelity=(abs((M*U.dag()).tr())^2)/(ISWAP.shape[1]*(ISWAP.shape[1]+1))
 
-  GaFid=(ptrace(sqrt(sqrt(ExGate)*DeGate*sqrt(ExgitGate))))^2
+phi=np.angle(M11)
+theta1=np.angle(M12)+(np.pi/2)-phi
+theta2=np.angle(M21)+(np.pi/2)-phi)
+
+r1=tensor(gSt,gSt) # |00>
+r2=tensor(gSt,eSt) # |01>
+r3=tensor(eSt,gSt) # |10>
+r4=tensor(eSt,eSt) # |11>
+
+U_iswap=[[np.exp((1j*phi)), 0, 0, 0],[0, 0, np.exp((1j*(-(np.pi/2)+theta1+phi))), 0],[0, np.exp((1j*(-(np.pi/2)+theta2+phi))), 0, 0],[0, 0, 0, np.exp((theta1+theta2+2*phi))]]
+
