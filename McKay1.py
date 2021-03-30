cd757f243cd4f4a29507df5101b16cfd63466f05
Emil Ingelsten <emilingelsten@MacBook-Pro-som-tillhor-Emil.local>
2021-03-30 23:37:58 +0200

    Test

--- McKay1.py
+++ McKay1.py
@@ -63,7 +63,7 @@ def getInitialGuess():
 
 def getParameterBounds():
     #Format of x: x = [Theta, delta, omegaPhi, omegaTB0]
-    return [(-0.5,0.5),(0,0.5),(0,5),(20,60)]
+    return [(-0.5,0.5),(0,0.3),(0,5),(20,60)]
 
 
 def timeEvolutionH1():
