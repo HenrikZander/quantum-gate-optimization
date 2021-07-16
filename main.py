509f01410bd9dded39aeb19d56c764a4b12e9256
Emil Ingelsten <emil.ingelsten@telia.com>
2021-07-16 12:03:24 +0200

    Checked the fidelity for one of the new solutions

--- main.py
+++ main.py
@@ -87,9 +87,10 @@ x_210713_iSWAP_1_4lvl = [4.03166181e-01, 2.16061627e-02, 3.50015579e+00, 7.98055
 x_210713_iSWAP_2_4lvl = [-0.2452259, 0.19856197, 3.52316351, 62.77634052] # Terrible: fidelity only 0.85
 x_210713_iSWAP_3_4lvl = [3.77667126e-01, 5.17617928e-02, 3.51608253e+00, 8.83255578e+01] # Bad: fidelity only 0.987
 x_210715_CZ_1_4lvl = [4.28265258e-01, 2.28050495e-02, 4.28583819e+00, 1.15226156e+02]
+x_210716_CZ_2_4lvl = [4.03140410e-01, 2.95892756e-02, 4.14970423e+00, 1.06035386e+02]
 # Solution to use in simulations: 
 #xUsed = x_210713_iSWAP_1_4lvl
-xName = 'x_210715_iSWAP_2_4lvl' # Preferred if solution is listed in solutions.json
+xName = 'x_210716_iSWAP_1_4lvl' # Preferred if solution is listed in solutions.json
 #dateStr = "2021-07-13 15:12:18.004557"
 
 ######################################################################################################################################################################
@@ -101,13 +102,13 @@ def main():
 
     # for index in range(64):
     #     findEigenIndex(xUsed, eigenStateIndex=index, N=4, printResult=True)
-    optimizeGate(CZ=True, energyLevels=4, maxAllowedGateTime=120, runDE=True, wantTradGate=False, wantCZ_20=False)
+    # optimizeGate(CZ=True, energyLevels=4, maxAllowedGateTime=120, runDE=True, wantTradGate=False, wantCZ_20=False)
     # optimizeGate(CZ=True, energyLevels=4, maxAllowedGateTime=240, runDE=True)
     # optimizeGate(CZ=True, energyLevels=4, maxAllowedGateTime=200, runDE=True)
     # optimizeGate(CZ=True, energyLevels=4, maxAllowedGateTime=170, runDE=True)
     # optimizeGate(CZ=True, energyLevels=4, maxAllowedGateTime=140, runDE=True)
     # simulateHamiltonian(xName=xName, sinStepHamiltonian=True, rotatingFrame=True, initialStateIndex=3, N=4, highestProjectionIndex=12)
-    # plotFidelity(xName=xName, wantCZ=True, useSavedPlot=False, saveTojson=True)
+    # plotFidelity(xName=xName, wantiSWAP=True, useSavedPlot=False, saveTojson=True)
     # deltaPulsePlot()
     # testPlotStates()
     # testEigenstateOrder()
